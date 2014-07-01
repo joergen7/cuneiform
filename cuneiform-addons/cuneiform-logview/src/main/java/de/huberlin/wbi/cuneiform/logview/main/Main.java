eb231a1043165766610f9a3b00a48a0d974a4d1b
Jörgen Brandt <joergen.brandt@onlinehome.de>
2014-07-01 14:16:41 +0200

    Comments improved.

--- cuneiform-addons/cuneiform-logview/src/main/java/de/huberlin/wbi/cuneiform/logview/main/Main.java
+++ cuneiform-addons/cuneiform-logview/src/main/java/de/huberlin/wbi/cuneiform/logview/main/Main.java
@@ -1,3 +1,35 @@
+/*******************************************************************************
+ * In the Hi-WAY project we propose a novel approach of executing scientific
+ * workflows processing Big Data, as found in NGS applications, on distributed
+ * computational infrastructures. The Hi-WAY software stack comprises the func-
+ * tional workflow language Cuneiform as well as the Hi-WAY ApplicationMaster
+ * for Apache Hadoop 2.x (YARN).
+ *
+ * List of Contributors:
+ *
+ * Jörgen Brandt (HU Berlin)
+ * Marc Bux (HU Berlin)
+ * Ulf Leser (HU Berlin)
+ *
+ * Jörgen Brandt is funded by the European Commission through the BiobankCloud
+ * project. Marc Bux is funded by the Deutsche Forschungsgemeinschaft through
+ * research training group SOAMED (GRK 1651).
+ *
+ * Copyright 2014 Humboldt-Universität zu Berlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package de.huberlin.wbi.cuneiform.logview.main;
 
 import java.io.BufferedReader;
