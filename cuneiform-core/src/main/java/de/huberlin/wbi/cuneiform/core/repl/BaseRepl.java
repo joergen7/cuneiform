c776bfbb6f82bd4e5617cbe3a889d7c8a95d1249
Jörgen Brandt <joergen.brandt@onlinehome.de>
2014-11-26 15:08:49 +0100

    Date altered.

--- cuneiform-core/src/main/java/de/huberlin/wbi/cuneiform/core/repl/BaseRepl.java
+++ cuneiform-core/src/main/java/de/huberlin/wbi/cuneiform/core/repl/BaseRepl.java
@@ -68,7 +68,7 @@ public abstract class BaseRepl {
 	public static final int CTL_TICKETSET = 8;
 
 	public static final String LABEL_VERSION = "2.0";
-	public static final String LABEL_BUILD = "2014-11-15";
+	public static final String LABEL_BUILD = "2014-11-26";
 
 	private final CfSemanticModelVisitor state;
 	private final Map<UUID,DynamicNodeVisitor> runningMap;
