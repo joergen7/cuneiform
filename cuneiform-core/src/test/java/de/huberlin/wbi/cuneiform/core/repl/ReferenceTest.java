f19aeafeacf8357f9e7ae26d21668bde503b0a1f
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-06-18 15:22:43 +0200

    Reference unit test class created.

---
+++ cuneiform-core/src/test/java/de/huberlin/wbi/cuneiform/core/repl/ReferenceTest.java
@@ -0,0 +1,94 @@
+package de.huberlin.wbi.cuneiform.core.repl;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.huberlin.wbi.cuneiform.core.semanticmodel.ApplyExpr;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.CompoundExpr;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.HasFailedException;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.NameExpr;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.NotDerivableException;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.QualifiedTicket;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.StringExpr;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.TopLevelContext;
+import de.huberlin.wbi.cuneiform.core.ticketsrc.NodeVisitorTicketSrc;
+
+public class ReferenceTest {
+
+	private DynamicNodeVisitor dnv;
+	private CompoundExpr x;
+	private TopLevelContext tlc;
+
+	@Before
+	public void setUp() throws HasFailedException, NotDerivableException {
+		
+		NodeVisitorTicketSrc ticketSrc;
+		BaseRepl repl;
+		QualifiedTicket qt;
+		
+		x = new CompoundExpr( new StringExpr( "Z" ) );
+		
+		qt = mock( QualifiedTicket.class );
+		when( qt.getOutputValue() ).thenThrow( new NotDerivableException( "blub" ) );
+		
+		ticketSrc = mock( NodeVisitorTicketSrc.class );
+		when( ticketSrc.requestTicket( any( BaseRepl.class ), any( UUID.class ), any( ApplyExpr.class ) ) ).
+		thenReturn( qt );
+		
+		repl = mock( BaseRepl.class );
+		tlc = new TopLevelContext();
+		tlc.putAssign( new NameExpr( "x" ), x );
+		
+		dnv = new DynamicNodeVisitor( ticketSrc, repl, tlc );
+	}
+	
+	@Test
+	public void nilShouldEvalItself() throws HasFailedException {
+		
+		CompoundExpr nil, result;
+		
+		nil = new CompoundExpr();
+		result = dnv.accept( nil );
+		assertEquals( nil, result );
+	}
+	
+	@Test
+	public void strShouldEvalItself() throws HasFailedException {
+		
+		CompoundExpr str, result;
+		
+		str = new CompoundExpr( new StringExpr( "bla" ) );
+		result = dnv.accept( str );
+		assertEquals( str, result );
+	}
+	
+	@Test( expected=HasFailedException.class )
+	public void undefVarShouldFail() throws HasFailedException {
+		
+		CompoundExpr freeVar;
+		
+		freeVar = new CompoundExpr( new NameExpr( "x" ) );
+		dnv.accept( freeVar );
+	}
+	
+	@Test
+	public void defVarShouldEvalToBoundValue() throws HasFailedException {
+		
+		CompoundExpr boundVar, result, content;
+		
+		content = new CompoundExpr( new StringExpr( "blub" ) );
+		tlc.putAssign( new NameExpr( "x" ), content );
+		
+		boundVar = new CompoundExpr( new NameExpr( "x" ) );
+		result = dnv.accept( boundVar );
+		
+		assertEquals( content, result );
+	}
+}
