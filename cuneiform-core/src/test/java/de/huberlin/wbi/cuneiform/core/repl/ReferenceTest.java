1d5f35550261e11ec3601db854d98591cc6ea30e
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-06-19 16:58:01 +0200

    More tests added.

--- cuneiform-core/src/test/java/de/huberlin/wbi/cuneiform/core/repl/ReferenceTest.java
+++ cuneiform-core/src/test/java/de/huberlin/wbi/cuneiform/core/repl/ReferenceTest.java
@@ -11,14 +11,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import de.huberlin.wbi.cuneiform.core.semanticmodel.ApplyExpr;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.Block;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.CompoundExpr;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.HasFailedException;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.NameExpr;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.NativeLambdaExpr;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.NotBoundException;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.NotDerivableException;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.Prototype;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.QualifiedTicket;
+import de.huberlin.wbi.cuneiform.core.semanticmodel.SemanticModelException;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.StringExpr;
-import de.huberlin.wbi.cuneiform.core.semanticmodel.Ticket;
 import de.huberlin.wbi.cuneiform.core.semanticmodel.TopLevelContext;
 import de.huberlin.wbi.cuneiform.core.ticketsrc.NodeVisitorTicketSrc;
 
@@ -117,5 +120,206 @@ public class ReferenceTest {
 		ce = new CompoundExpr( );
 		
 		assertEquals( ce, dnv.accept( ce ) );
+	}
+	
+	@Test
+	public void identityFnShouldEvalArg() throws HasFailedException, NotBoundException {
+		
+		CompoundExpr e, f, lamList;
+		Prototype sign;
+		Block body;
+		ApplyExpr ae;
+		
+		
+		e = new CompoundExpr( new StringExpr( "bla" ) );
+		
+		sign = new Prototype();
+		sign.addOutput( new NameExpr( "out" ) );
+		sign.addParam( new NameExpr( "task" ) );
+		sign.addParam( new NameExpr( "inp" ) );
+		
+		body = new Block();
+		body.putAssign( new NameExpr( "out" ), new CompoundExpr( new NameExpr( "inp" ) ) );
+		
+		lamList = new CompoundExpr( new NativeLambdaExpr( sign, body ) );
+		
+		ae = new ApplyExpr( 1, false );
+		ae.setTaskExpr( lamList );
+		ae.putAssign( new NameExpr( "inp" ), e );
+		
+		f = new CompoundExpr( ae );
+		
+		assertEquals( e, dnv.accept( f ) );
+	}
+	
+	@Test
+	public void lamShouldEvalItself() throws HasFailedException, NotBoundException {
+		
+		Prototype sign;
+		Block body;
+		CompoundExpr lamList;
+		
+		sign = new Prototype();
+		sign.addOutput( new NameExpr( "out" ) );
+		sign.addParam( new NameExpr( "task" ) );
+		
+		body = new Block();
+		body.putAssign( new NameExpr( "out" ), new CompoundExpr( new NameExpr( "blub" ) ) );
+		
+		lamList = new CompoundExpr( new NativeLambdaExpr( sign, body ) );
+		
+		assertEquals( lamList, dnv.accept( lamList ) );
+	}
+	
+	@Test
+	public void multipleOutputShouldBeBindable() throws HasFailedException, NotBoundException {
+		
+		Prototype sign;
+		CompoundExpr e1, e2, lamList;
+		ApplyExpr f1, f2;
+		Block body;
+		
+		
+		sign = new Prototype();
+		sign.addOutput( new NameExpr( "out1" ) );
+		sign.addOutput( new NameExpr( "out2" ) );
+		sign.addParam( new NameExpr( "task" ) );
+		
+		e1 = new CompoundExpr( new StringExpr( "bla" ) );
+		e2 = new CompoundExpr( new StringExpr( "blub" ) );
+		
+		body = new Block();
+		body.putAssign( new NameExpr( "out1" ), e1 );
+		body.putAssign( new NameExpr( "out2" ), e2 );
+		
+		lamList = new CompoundExpr( new NativeLambdaExpr( sign, body ) );
+		
+		f1 = new ApplyExpr( 1, false );
+		f1.setTaskExpr( lamList );
+		
+		f2 = new ApplyExpr( 2, false );
+		f2.setTaskExpr( lamList );
+		
+		assertEquals( e1, dnv.accept( f1 ) );
+		assertEquals( e2, dnv.accept( f2 ) );
+	}
+	
+	@Test( expected=SemanticModelException.class )
+	public void applicationShouldIgnoreCallingContext() throws HasFailedException, NotBoundException {
+		
+		Prototype sign;
+		Block body;
+		CompoundExpr lamList;
+		ApplyExpr ae;
+		
+		sign = new Prototype();
+		sign.addOutput( new NameExpr( "out" ) );
+		sign.addParam( new NameExpr( "task" ) );
+		
+		body = new Block();
+		body.putAssign( new NameExpr( "out" ), new CompoundExpr( new NameExpr( "x" ) ) );
+		
+		lamList = new CompoundExpr( new NativeLambdaExpr( sign, body ) );
+		
+		ae = new ApplyExpr( 1, false );
+		ae.setTaskExpr( lamList );
+		
+		tlc.putAssign( new NameExpr( "x" ), new CompoundExpr( new StringExpr( "blub" ) ) );
+		dnv.accept( new CompoundExpr( ae ) );
+	}
+	
+	@Test
+	public void bindingShouldOverrideBody() throws HasFailedException, NotBoundException {
+		
+		CompoundExpr f, g, h, lamList;
+		Prototype sign;
+		Block body;
+		ApplyExpr ae;
+		
+		f = new CompoundExpr( new StringExpr( "blub" ) );
+		
+		sign = new Prototype();
+		sign.addOutput( new NameExpr( "out" ) );
+		sign.addParam( new NameExpr( "task" ) );
+		sign.addParam( new NameExpr( "x" ) );
+		
+		body = new Block();
+		body.putAssign( new NameExpr( "x" ), new CompoundExpr( new StringExpr( "bla" ) ) );
+		body.putAssign( new NameExpr( "out" ), new CompoundExpr( new NameExpr( "x" ) ) );
+		
+		lamList = new CompoundExpr( new NativeLambdaExpr( sign, body ) );
+		
+		ae = new ApplyExpr( 1, false );
+		ae.setTaskExpr( lamList );
+		ae.putAssign( new NameExpr( "x" ), f );
+		
+		g = new CompoundExpr( ae );
+		h = dnv.accept( g );
+		
+		assertEquals( f, h );
+	}
+	
+	@Test( expected=NullPointerException.class )
+	public void appWithEmptyTaskListShouldFail() throws HasFailedException, NotBoundException {
+		
+		ApplyExpr ae;
+		
+		ae = new ApplyExpr( 1, false );
+		dnv.accept( new CompoundExpr( ae ) );
+	}
+	
+	@Test
+	public void crossProductShouldBeDerivableFromSignature() throws HasFailedException, NotBoundException {
+		
+		Prototype sign;
+		CompoundExpr e1, e2, lamList, f1, f2;
+		Block body;
+		ApplyExpr ae1, ae2;
+		
+		e1 = new CompoundExpr();
+		e1.addSingleExpr( new StringExpr( "A" ) );
+		e1.addSingleExpr( new StringExpr( "B" ) );
+		
+		e2 = new CompoundExpr();
+		e2.addSingleExpr( new StringExpr( "1" ) );
+		e2.addSingleExpr( new StringExpr( "2" ) );
+		
+		sign = new Prototype();
+		sign.addOutput( new NameExpr( "out1" ) );
+		sign.addOutput( new NameExpr( "out2" ) );
+		sign.addParam( new NameExpr( "task" ) );
+		sign.addParam( new NameExpr( "p1" ) );
+		sign.addParam( new NameExpr( "p2" ) );
+		
+		body = new Block();
+		body.putAssign( new NameExpr( "out1" ), new CompoundExpr( new NameExpr( "p1" ) ) );
+		body.putAssign( new NameExpr( "out2" ), new CompoundExpr( new NameExpr( "p2" ) ) );
+		
+		lamList = new CompoundExpr( new NativeLambdaExpr( sign, body ) );
+		
+		ae1 = new ApplyExpr( 1, false );
+		ae1.setTaskExpr( lamList );
+		ae1.putAssign( new NameExpr( "p1" ), e1 );
+		ae1.putAssign( new NameExpr( "p2" ), e2 );
+		
+		ae2 = new ApplyExpr( 2, false );
+		ae2.setTaskExpr( lamList );
+		ae2.putAssign( new NameExpr( "p1" ), e1 );
+		ae2.putAssign( new NameExpr( "p2" ), e2 );
+		
+		f1 = new CompoundExpr();
+		f1.addSingleExpr( new StringExpr( "A" ) );
+		f1.addSingleExpr( new StringExpr( "A" ) );
+		f1.addSingleExpr( new StringExpr( "B" ) );
+		f1.addSingleExpr( new StringExpr( "B" ) );
+		
+		f2 = new CompoundExpr();
+		f2.addSingleExpr( new StringExpr( "1" ) );
+		f2.addSingleExpr( new StringExpr( "2" ) );
+		f2.addSingleExpr( new StringExpr( "1" ) );
+		f2.addSingleExpr( new StringExpr( "2" ) );
+		
+		assertEquals( f1, dnv.accept( new CompoundExpr( ae1 ) ) );
+		assertEquals( f2, dnv.accept( new CompoundExpr( ae2 ) ) );
 	}
 }
