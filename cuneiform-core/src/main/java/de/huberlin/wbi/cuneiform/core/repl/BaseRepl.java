943c0a400de56ba27b12e76e4c9826202847620c
Jorgen Brandt <joergen.brandt@onlinehome.de>
2016-04-11 19:52:34 +0200

    2.0.4 release

--- cuneiform-core/src/main/java/de/huberlin/wbi/cuneiform/core/repl/BaseRepl.java
+++ cuneiform-core/src/main/java/de/huberlin/wbi/cuneiform/core/repl/BaseRepl.java
@@ -71,8 +71,8 @@ public abstract class BaseRepl {
 	public static final int CTL_QUERYSET = 4;
 	public static final int CTL_TICKETSET = 8;
 
-	public static final String LABEL_VERSION = "2.0.4-SNAPSHOT";
-	public static final String LABEL_BUILD = "2016-02-08";
+	public static final String LABEL_VERSION = "2.0.4-RELEASE";
+	public static final String LABEL_BUILD = "2016-04-11";
 
 	private final CfSemanticModelVisitor state;
 	private final Map<UUID,DynamicNodeVisitor> runningMap;
