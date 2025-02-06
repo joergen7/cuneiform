4c0b05b30af5fdcf5ff3fd0ecbe01c0a0b51e24f
Jörgen Brandt <joergen@cuneiform-lang.org>
2025-02-06 15:41:47 +0100

    quality of life

--- src/cuneiform.erl
+++ src/cuneiform.erl
@@ -28,153 +28,152 @@
 %% @end
 %% -------------------------------------------------------------------
 
--module( cuneiform ).
+-module(cuneiform).
 
--export( [main/1] ).
+-export([main/1]).
 
--define( VSN, "3.0.6" ).
+-define(VSN, "3.0.6").
 
 %%====================================================================
 %% Escript main function
 %%====================================================================
 
-main( Args ) ->
 
-  % TODO: neutralize all configurations
+main(Args) ->
 
-  try
+    % TODO: neutralize all configurations
 
-    case getopt:parse( get_optspec_lst(), Args ) of
-  
-      {error, R1} ->
-        throw( {error, R1} );
+    try
 
-      {ok, {OptLst, NonOptLst}} ->
+        case getopt:parse(get_optspec_lst(), Args) of
 
-        % break if version needs to be displayed
-        case lists:member( version, OptLst ) of
-          false -> ok;
-          true  -> throw( version )
-        end,
+            {error, R1} ->
+                throw({error, R1});
 
-        % break if help needs to be displayed
-        case lists:member( help, OptLst ) of
-          false -> ok;
-          true  -> throw( help )
-        end,
+            {ok, {OptLst, NonOptLst}} ->
 
-        % extract number of workers
-        M1 =
-          case lists:keyfind( n_wrk, 1, OptLst ) of
-            false                 -> #{ n_wrk => <<"auto">> };
-            {n_wrk, N} when N > 0 -> #{ n_wrk => N };
-            A                     -> throw( {error, {invalid_arg, A}} )
-          end,
+                % break if version needs to be displayed
+                case lists:member(version, OptLst) of
+                    false -> ok;
+                    true -> throw(version)
+                end,
 
-        % extract working directory
-        M2 =
-          case lists:keyfind( wrk_dir, 1, OptLst ) of
-            false        -> M1#{ wrk_dir => <<"./_cuneiform/wrk">> };
-            {wrk_dir, W} -> M1#{ wrk_dir => W }
-          end,
+                % break if help needs to be displayed
+                case lists:member(help, OptLst) of
+                    false -> ok;
+                    true -> throw(help)
+                end,
 
-        % extract repository directory
-        M3 =
-          case lists:keyfind( repo_dir, 1, OptLst ) of
-            false         -> M2#{ repo_dir => <<"./_cuneiform/repo">> };
-            {repo_dir, R} -> M2#{ repo_dir => R }
-          end,
+                % extract number of workers
+                M1 =
+                    case lists:keyfind(n_wrk, 1, OptLst) of
+                        false -> #{n_wrk => <<"auto">>};
+                        {n_wrk, N} when N > 0 -> #{n_wrk => N};
+                        A -> throw({error, {invalid_arg, A}})
+                    end,
 
-        % extract data directory
-        M4 =
-          case lists:keyfind( data_dir, 1, OptLst ) of
-            false         -> M3#{ data_dir => <<".">> };
-            {data_dir, D} -> M3#{ data_dir => D }
-          end,
+                % extract working directory
+                M2 =
+                    case lists:keyfind(wrk_dir, 1, OptLst) of
+                        false -> M1#{wrk_dir => <<"./_cuneiform/wrk">>};
+                        {wrk_dir, W} -> M1#{wrk_dir => W}
+                    end,
 
-        % set flag maps
-        ok = application:set_env( cf_client, flag_map, #{ cre_node => <<"node">> } ),
-        ok = application:set_env( cf_worker, flag_map, M4#{ cre_node => <<"node">> } ),
+                % extract repository directory
+                M3 =
+                    case lists:keyfind(repo_dir, 1, OptLst) of
+                        false -> M2#{repo_dir => <<"./_cuneiform/repo">>};
+                        {repo_dir, R} -> M2#{repo_dir => R}
+                    end,
 
-        % start CRE application
-        ok = cre:start(),
+                % extract data directory
+                M4 =
+                    case lists:keyfind(data_dir, 1, OptLst) of
+                        false -> M3#{data_dir => <<".">>};
+                        {data_dir, D} -> M3#{data_dir => D}
+                    end,
 
-        % start worker application
-        ok = cf_worker:start(),
+                % set flag maps
+                ok = application:set_env(cf_client, flag_map, #{cre_node => <<"node">>}),
+                ok = application:set_env(cf_worker, flag_map, M4#{cre_node => <<"node">>}),
 
-        % start client application
-        ok = cf_client:start(),
+                % start CRE application
+                ok = cre:start(),
 
-        case NonOptLst of
-          []    -> throw( shell );
-          [_|_] -> throw( {load, NonOptLst} )
+                % start worker application
+                ok = cf_worker:start(),
+
+                % start client application
+                ok = cf_client:start(),
+
+                case NonOptLst of
+                    [] -> throw(shell);
+                    [_ | _] -> throw({load, NonOptLst})
+                end
+
         end
 
-    end
+    catch
 
-  catch
+        throw:version ->
+            print_version();
 
-    throw:version ->
-      print_version();
+        throw:help ->
+            print_help();
 
-    throw:help ->
-      print_help();
+        throw:shell ->
+            print_version(),
+            ok = cuneiform_shell:shell(cf_client);
 
-    throw:shell ->
-      print_version(),
-      ok = cuneiform_shell:shell( cf_client );
+        throw:{load, FileLst} ->
 
-    throw:{load, FileLst} ->
+            F =
+                fun(File) ->
 
-      F =
-        fun( File ) ->
+                        % collect reply list
+                        ReplyLst =
+                            case file:read_file(File) of
 
-          % collect reply list
-          ReplyLst =
-            case file:read_file( File ) of
+                                {error, R3} ->
+                                    io:format("we land here~n"),
+                                    [{error, load, {File, R3}}];
 
-              {error, R3} ->
-                io:format( "we land here~n" ),
-                [{error, load, {File, R3}}];
+                                {ok, B} ->
+                                    S = binary_to_list(B),
+                                    cuneiform_shell:shell_eval_oneshot(S)
 
-              {ok, B} ->
-                S = binary_to_list( B ),
-                cuneiform_shell:shell_eval_oneshot( S )
+                            end,
 
-            end,
+                        cuneiform_shell:process_reply_lst(ReplyLst, cf_client, silent)
 
-          cuneiform_shell:process_reply_lst( ReplyLst, cf_client, silent )
+                end,
 
-        end,
+            ok = lists:foreach(F, FileLst);
 
-      ok = lists:foreach( F, FileLst );
+        throw:{error, Reason} ->
+            ReplyLst = [{error, load, Reason}],
+            cuneiform_shell:process_reply_lst(ReplyLst, cf_client, silent)
 
-    throw:{error, Reason} ->
-      ReplyLst = [{error, load, Reason}],
-      cuneiform_shell:process_reply_lst( ReplyLst, cf_client, silent )
-
-  end.
-
+    end.
 
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
+
 get_optspec_lst() ->
-  [
-   {version,    $v, "version",    undefined, "Show cf_worker version."},
-   {help,       $h, "help",       undefined, "Show command line options."},
-   {n_wrk,      $n, "n_wrk",      integer,   "Number of worker processes to start. 0 means auto-detect available processors."},
-   {wrk_dir,    $w, "wrk_dir",    binary,    "Working directory in which workers store temporary files."},
-   {repo_dir,   $r, "repo_dir",   binary,    "Repository directory for intermediate and output data."},
-   {data_dir,   $d, "data_dir",   binary,    "Data directory where input data is located."}
-  ].
+    [{version, $v, "version", undefined, "Show cf_worker version."},
+     {help, $h, "help", undefined, "Show command line options."},
+     {n_wrk, $n, "n_wrk", integer, "Number of worker processes to start. 0 means auto-detect available processors."},
+     {wrk_dir, $w, "wrk_dir", binary, "Working directory in which workers store temporary files."},
+     {repo_dir, $r, "repo_dir", binary, "Repository directory for intermediate and output data."},
+     {data_dir, $d, "data_dir", binary, "Data directory where input data is located."}].
+
 
 print_help() ->
-  getopt:usage( get_optspec_lst(), "cfl" ).
+    getopt:usage(get_optspec_lst(), "cfl").
+
 
 print_version() ->
-  io:format( "cuneiform ~s~n", [?VSN] ).
-
-
+    io:format("cuneiform ~s~n", [?VSN]).
